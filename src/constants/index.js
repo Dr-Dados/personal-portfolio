47f7b36d1b435f2ad3d2db627d423310134321bd
Dr.Dados <adnane.bachchar@gmail.com>
2024-07-04 11:36:53 +0100

    adding contact infos

--- src/constants/index.js
+++ src/constants/index.js
@@ -88,7 +88,7 @@ export const PROJECTS = [
 ];
 
 export const CONTACT = {
-  address: "767 Fifth Avenue, New York, NY 10153 ",
-  phoneNo: "+12 4555 666 00 ",
-  email: "me@example.com",
+  address: "Casablanca, Morocco ",
+  phoneNo: "+212 660 052 931 ",
+  email: "adnane.bachchar@gmail.com",
 };
