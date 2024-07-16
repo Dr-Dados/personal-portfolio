7ef100b2cbe5efd417f1785f3a8a8d8a722d1099
Dr.Dados <adnane.bachchar@gmail.com>
2024-07-16 11:21:18 +0100

    adding projects and experiences

--- src/constants/index.js
+++ src/constants/index.js
@@ -9,43 +9,10 @@ export const ABOUT_TEXT = `I am a committed and adaptable full stack developer w
 
 export const EXPERIENCES = [
   {
-    year: "2023 - Present",
-    role: "Senior Full Stack Developer",
-    company: "Google Inc.",
-    description: `Led a team in developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
-    technologies: [
-      "JavaScript",
-      "React.js",
-      "Node.js",
-      "Express.js",
-      "MongoDB",
-    ],
-  },
-  {
-    year: "2022 - 2023",
-    role: "Frontend Developer",
-    company: "Adobe",
-    description: `Designed and developed user interfaces for web applications using React.js. Worked closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
-    technologies: ["HTML", "CSS", "React.js", "Node.js", "MongoDB"],
-  },
-  {
-    year: "2021 - 2022",
-    role: "Full Stack Developer",
-    company: "Facebook",
-    description: `Developed and maintained web applications using JavaScript, React.js, and Node.js. Designed and implemented RESTful APIs for data communication. Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
-    technologies: [
-      "JavaScript",
-      "React.js",
-      "Node.js",
-      "Express.js",
-      "MongoDB",
-    ],
-  },
-  {
-    year: "2020 - 2021",
-    role: "Software Engineer",
-    company: "Paypal",
-    description: `Contributed to the development of web applications using JavaScript, React.js, and Node.js. Managed databases and implemented data storage solutions using MongoDB. Worked closely with product managers to prioritize features and enhancements.`,
+    year: "2021 - Present",
+    role: "Full Stack Developer & web master",
+    company: "Medipro Group.",
+    description: `developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with team managers to define project requirements and timelines.`,
     technologies: [
       "JavaScript",
       "React.js",
@@ -58,32 +25,48 @@ export const EXPERIENCES = [
 
 export const PROJECTS = [
   {
-    title: "E-Commerce Website",
+    title: "Hotel Manager",
     image: project1,
     description:
-      "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
-    technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
+      "an application that lets you manage apartments and rentals, view availability, add and delete apartments and create users",
+    technologies: ["HTML", "CSS", "React", "Node.js", "Supabase"],
   },
   {
     title: "Task Management App",
     image: project2,
     description:
       "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
-    technologies: ["HTML", "CSS", "Angular", "Firebase"],
+    technologies: [
+      "HTML",
+      "CSS",
+      "ReactJs",
+      "MongoDB",
+      "Node.js",
+      "Express.js",
+    ],
   },
   {
     title: "Portfolio Website",
     image: project3,
     description:
       "A personal portfolio website showcasing projects, skills, and contact information.",
-    technologies: ["HTML", "CSS", "React", "Bootstrap"],
+    technologies: ["HTML", "CSS", "React", "TailwindCss"],
   },
   {
-    title: "Blogging Platform",
+    title: "BL Manager Platform",
     image: project4,
     description:
-      "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
-    technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"],
+      "an application that simplifies communication between staff. It makes it easy to manage delivery notes and acknowledgement of receipt. It also allows delegates to leave comments and notes on delivery notes received. ",
+    technologies: [
+      "HTML",
+      "CSS",
+      "React.js",
+      "Express",
+      "MongoDB",
+      "TailWindCss",
+      "Node.js",
+      "Express.js",
+    ],
   },
 ];
 
